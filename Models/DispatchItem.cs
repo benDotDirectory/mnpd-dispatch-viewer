a47952fc123e7838722fd4626418ce6e1485dabd
ben <bendotbike@gmail.com>
2022-07-07 18:20:23 -0500

    Add RestService, DispatchItem model, modify main page to show output
    - Create Constants.cs with json endpoint
    - Modify MainPage.xaml to show first item that comes back from web request
    - Add Newtonsoft Json.NET package for json serialization/deserialization
    - Add DispatchItem model
    - Add RestService to make requests to endpoint

---
+++ Models/DispatchItem.cs
@@ -0,0 +1,12 @@
+﻿namespace mnpd_dispatch_viewer.Models
+{
+    public class DispatchItem
+    {
+        public string Address { get; set; }
+        public string Call_Received { get; set; }
+        public string City { get; set; }
+        public string Incident_Type { get; set; }
+        public string Incident_Type_Code { get; set; }
+        public string Last_Updated { get; set; }
+    }
+}
---
+++ Services/RestService.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+using mnpd_dispatch_viewer.Models;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+
+namespace mnpd_dispatch_viewer.Services
+{
+    public class RestService
+    {
+        HttpClient Client;
+        JsonSerializerOptions SerializerOptions;
+
+        public List<DispatchItem> Items { get; set; }
+
+        public RestService()
+        {
+            Client = new HttpClient();
+            SerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+        }
+
+        public async Task<List<DispatchItem>> RefreshDataAsync()
+        {
+            Items = new List<DispatchItem>();
+
+            Uri uri = new Uri(string.Format(Constants.MNPDJsonLink, string.Empty));
+
+            try
+            {
+                HttpResponseMessage response = await Client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string body = await response.Content.ReadAsStringAsync();
+                    Items = JsonConvert.DeserializeObject<List<DispatchItem>>(body);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return Items;
+        }
+    }
+}
